4613cc574f38becc36a8aeeecb14d55398193ed5
ckyle121 <carolinekyle21@gmail.com>
2022-05-02 09:18:52 -0500

    add mutations for post and friend

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -32,6 +32,14 @@ type User {
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
   }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
 `;
 
 module.exports = typeDefs;
